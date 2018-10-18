f0a57ca634958b40a426e28f468637aa9bd25486
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-10-17 22:45:03 -0400

    Added a page to handle adding a new record

---
+++ src/NewEntry.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { push } from 'redux-router';
+import { newEntry } from './actions';
+
+export class NewEntry extends Component {
+  constructor() {
+    super();
+    this.handleOnSave = this.handleOnSave.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <input type="text" placeholder="Title" ref={input => (this.titleRef = input)} />
+        </div>
+
+        <div>
+          Entry:
+          <textarea name="" id="" cols="30" rows="10" ref={input => (this.bodyRef = input)} />
+        </div>
+
+        <div>
+          <button onClick={this.handleOnSave}>Save</button>
+        </div>
+      </div>
+    );
+  }
+
+  handleOnSave() {
+    const entry = {
+      title: this.titleRef.value,
+      body: this.bodyRef.value,
+    };
+    return this.props.onSave(entry);
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSave: entry => {
+      console.log('Saving entry', entry);
+      dispatch(newEntry(entry));
+      dispatch(push('/'));
+    },
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(NewEntry);
